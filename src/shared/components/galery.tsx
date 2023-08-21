22b9a6e79d7a814f828becd5c4430d518df3602b
jampoolg <jampoolgarcia@gmail.com>
2023-08-20 22:56:42 -0400

    update galery

--- src/shared/components/galery.tsx
+++ src/shared/components/galery.tsx
@@ -1,20 +1,31 @@
 import { component$ } from "@builder.io/qwik";
 
 export const GalerySection = component$(() => {
-    return (<>
-        <section class="bg-[#3d83ff] h-screen w-full flex flex-col justify-around items-center gap-4 p-4 text-white">
 
-            <section  class="flex w-full h-full bg-red-500">
-                <img width={0} height={0} class="flex-grow object-cover opacity-80 transition-all ease-in-out duration-500 hover:cursor-crosshair hover:w-2/3 hover:opacity-100" src="https://images.unsplash.com/photo-1422190441165-ec2956dc9ecc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80" alt="" /> 
-                <img width={0} height={0} class="flex-grow object-cover opacity-80 transition-all  ease-in-out duration-500 hover:cursor-crosshair hover:w-2/3 hover:opacity-100" src="https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjM0MTM2fQ&auto=format&fit=crop&w=1600&q=80" alt="" /> 
-                <img width={0} height={0} class="flex-grow object-cover opacity-80 transition-all  ease-in-out duration-500 hover:cursor-crosshair hover:w-2/3 hover:opacity-100" src="https://images.unsplash.com/photo-1519327232521-1ea2c736d34d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80" alt="" /> 
+    const imgs = [
+        'https://images.unsplash.com/photo-1422190441165-ec2956dc9ecc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80',
+        'https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjM0MTM2fQ&auto=format&fit=crop&w=1600&q=80',
+        'https://images.unsplash.com/photo-1519327232521-1ea2c736d34d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80'
+    ]
+
+    return (<>
+        <section id="galery" class="bg-[#3d83ff] h-screen w-full flex flex-col lg:flex-col-reverse justify-around items-center gap-4 p-4 text-white">
+
+            <section  class="flex w-full lg:w-2/3 h-full lg:h-2/3 bg-red-500">
+                {
+                    imgs.map(img => (
+                        <img width={0} height={0} 
+                        class="flex-grow object-cover opacity-80 transition-all ease-in-out duration-500 hover:cursor-crosshair hover:w-2/3 hover:opacity-100" 
+                        src={img} alt="images" /> 
+                    ))
+                }
             </section>  
 
             <div>
                 <h1 class="text-4xl text-center font-semibold sm:pr-8 xl:pr-12">Galery</h1>
-                <p class="text-sm xl:text-base text-center my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Temporibus eligendi accusamus laboriosam vero voluptatibus, quos ut qui saepe delectus nam sed quaerat! 
-                    Quidem et, quis rerum deserunt reprehenderit dignissimos fugit!
+                <p class="text-sm xl:text-base text-center my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/> 
+                    Temporibus eligendi accusamus laboriosam vero voluptatibus,<br/> quos ut qui saepe delectus nam sed quaerat! 
+                    Quidem et, <br/>quis rerum deserunt reprehenderit dignissimos fugit!
                 </p>
             </div>
 
