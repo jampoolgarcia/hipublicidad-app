c0651bb6e56620f02b1f464f78a543c427123375
jampoolg <jampoolgarcia@gmail.com>
2023-08-25 07:14:55 -0400

    repair last use method error

--- src/routes/context.provaider.tsx
+++ src/routes/context.provaider.tsx
@@ -1,11 +1,28 @@
-import { Slot, component$ } from "@builder.io/qwik";
-import { productProvaider } from "~/product/context/product.provaider";
-import { cartProvaider } from "~/shopping-cart/context";
+import { Slot, component$, useContextProvider, useStore } from "@builder.io/qwik";
+import { type IProductsState, productsContext } from "~/product/context/product.context";
+import { CartContext, type ICartState } from "~/shopping-cart/context";
+
 
 export const ContextProvaider = component$(() => {
 
-    cartProvaider();
-    productProvaider();
+     // creamos el estado por defecto
+     const cart = useStore<ICartState>({
+        id: 1,
+        userId: 1,
+        date: new Date('2020-03-02T00:00:00.000Z'),
+        products: []
+    })
+
+    // proveemos el contexto en la app
+    useContextProvider(CartContext, cart);
+
+    // creamos el estado por defecto
+    const productsState = useStore<IProductsState>({
+        products: []
+    })
+
+    // proveemos el contexto en la app
+    useContextProvider(productsContext, productsState);
 
     return (<Slot />)
 })
