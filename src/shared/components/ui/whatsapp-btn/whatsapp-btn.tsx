f846f3e47d604935b3dd82378c890bc979917796
jampoolg <jampoolgarcia@gmail.com>
2023-10-08 09:44:26 -0300

    update whatsapp

--- src/shared/components/ui/whatsapp-btn/whatsapp-btn.tsx
+++ src/shared/components/ui/whatsapp-btn/whatsapp-btn.tsx
@@ -1,7 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 import { phoneNumber } from "~/helpers/config";
-import { Image } from '@unpic/qwik';
 
 
 export const WhatsApp = component$(() => {
@@ -12,7 +11,7 @@ export const WhatsApp = component$(() => {
     return (<Link 
                 href={whatsapp} 
                 target="_blank"
-                class=" bg-white fixed flex justify-center items-center z-[1001] bottom-3 right-3 md:bottom-12 md:right-12 w-16 h-16 rounded-full cursor-pointer transition-transform duration-500 hover:scale-105 custom-shadow">
-                    <Image width="64"  height="64" src="/whatsapp.png" alt="whatsapp" />
+                class=" bg-[#39d254] fixed flex justify-center items-center z-[1001] bottom-3 right-3 md:bottom-12 md:right-12 w-16 h-16 rounded-full cursor-pointer transition-transform duration-500 hover:scale-105 custom-shadow">
+                    <i class="fa-brands fa-whatsapp text-5xl" style="color: #ffffff;"></i>
             </Link>)
 })
