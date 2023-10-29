477b1aad521c56a79a3a90126eb1e0f1054f84fa
JampoolG <jampoolgarcia@gmail.com>
2023-10-29 07:53:31 -0300

    update error swiper

--- src/shared/components/carousel/carousel.tsx
+++ src/shared/components/carousel/carousel.tsx
@@ -1,6 +1,12 @@
-import { component$, useStyles$ } from "@builder.io/qwik"
+import { Slot, component$, useStyles$, useVisibleTask$ } from "@builder.io/qwik"
 
-
+// core version + navigation, pagination modules:
+import Swiper from 'swiper'
+import { Navigation, Controller, EffectCoverflow, Grid, A11y, FreeMode } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 
@@ -11,11 +17,35 @@ export const CarouselImg = component$(() => {
 
   useStyles$(style);
 
+  useVisibleTask$(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      modules: [Navigation, EffectCoverflow, Grid, FreeMode, A11y, Controller ],
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      autoplay: true,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+      }
+    });
+  })
 
   return (<>
+
     <div class="section">
-     
+      <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+          <Slot />
+        </div>
+      </div>
     </div>
+
   </>)
 })
 
--- src/shared/components/section/galery/galery.tsx
+++ src/shared/components/section/galery/galery.tsx
@@ -3,6 +3,7 @@ import { component$, useSignal } from "@builder.io/qwik";
 
 import { PageTitle } from "~/shared";
 
+import { CarouselImg } from "../../carousel/carousel";
 import { Image } from '@unpic/qwik';
 import { imgGaleryUrl } from "~/helpers/config";
 
@@ -27,39 +28,17 @@ export const GalerySection = component$(() => {
                 </p>
             </div>
 
+            
 
-            <div id="custom-controls-gallery" class="relative w-full" data-carousel="slide">
-              
-                <div class="relative h-56 overflow-hidden rounded-lg md:h-120">
-
-                    {
-                        imgs.value.map((img) => (
-                            <div  key={img}  class="hidden duration-700 ease-in-out" data-carousel-item>
-                                <Image width="1280" height="1040" src={`${imgGaleryUrl}${img}`} loading="lazy" alt={img}  class="absolute block w-full max-w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
-                            </div>
-                        ))
-                    }
-                    
-                </div>
-                <div class="flex justify-center items-center pt-4">
-                    <button type="button" class="flex justify-center items-center mr-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-                        <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
-                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
-                            </svg>
-                            <span class="sr-only">Previous</span>
-                        </span>
-                    </button>
-                    <button type="button" class="flex justify-center items-center h-full cursor-pointer group focus:outline-none" data-carousel-next>
-                        <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
-                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
-                            <span class="sr-only">Next</span>
-                        </span>
-                    </button>
-                </div>
-            </div>
+            <CarouselImg>
+                {
+                    imgs.value.map((img) => (
+                        <div key={img} class="swiper-slide">
+                            <Image width="400" height="400" src={`${imgGaleryUrl}${img}`} loading="lazy" alt={img} />
+                        </div>
+                    ))
+                }
+            </CarouselImg>
 
 
         </section>
