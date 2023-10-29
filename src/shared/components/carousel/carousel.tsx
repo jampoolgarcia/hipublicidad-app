1297aab238ab35a2905e3ef397b98627b74e2b3f
JampoolG <jampoolgarcia@gmail.com>
2023-10-29 07:49:23 -0300

    update error

--- src/shared/components/carousel/carousel.tsx
+++ src/shared/components/carousel/carousel.tsx
@@ -1,14 +1,8 @@
-import { component$, useStyles$, useVisibleTask$ } from "@builder.io/qwik"
+import { component$, useStyles$ } from "@builder.io/qwik"
 
-// core version + navigation, pagination modules:
-import Swiper from 'swiper'
-import { Navigation, Pagination, Controller, EffectCoverflow, Grid, A11y, Lazy, Autoplay, FreeMode } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
-import { Image } from '@unpic/qwik';
+
+
 
 import style from './carousel.css?inline';
 
@@ -17,95 +11,11 @@ export const CarouselImg = component$(() => {
 
   useStyles$(style);
 
-  useVisibleTask$(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const swiper = new Swiper(".mySwiper", {
-      effect: "coverflow",
-      modules: [Navigation, EffectCoverflow, Grid, FreeMode, A11y, Controller ],
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-      autoplay: true,
-      coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 2,
-        slideShadows: true,
-      }
-    });
-  })
 
   return (<>
-
-    <section>
-      <div class="swiper mySwiper">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <Image src="https://swiperjs.com/demos/images/nature-1.jpg" loading="lazy" />
-          </div>
-          <div class="swiper-slide">
-            <Image src="https://swiperjs.com/demos/images/nature-2.jpg" loading="lazy" />
-          </div>
-          <div class="swiper-slide">
-            <Image src="https://swiperjs.com/demos/images/nature-3.jpg"  loading="lazy" />
-          </div>
-          <div class="swiper-slide">
-            <Image src="https://swiperjs.com/demos/images/nature-4.jpg"  loading="lazy" />
-          </div>
-          <div class="swiper-slide">
-            <Image src="https://swiperjs.com/demos/images/nature-5.jpg"  loading="lazy"/>
-          </div>
-          <div class="swiper-slide">
-            <Image src="https://swiperjs.com/demos/images/nature-6.jpg"  loading="lazy"  />
-          </div>
-          <div class="swiper-slide">
-            <Image src="https://swiperjs.com/demos/images/nature-7.jpg"  loading="lazy"  />
-          </div>
-          <div class="swiper-slide">
-            <Image src="https://swiperjs.com/demos/images/nature-8.jpg"  loading="lazy"/>
-          </div>
-          <div class="swiper-slide">
-            <Image src="https://swiperjs.com/demos/images/nature-9.jpg" loading="lazy"  />
-          </div>
-        </div>
-      </div>
-    </section>
-
-
-    {/* <div class="swiper mySwiper">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide"> 
-        <Image src="https://swiperjs.com/demos/images/nature-1.jpg" loading="lazy" />
-      </div>
-      <div class="swiper-slide">
-        <Image src="https://swiperjs.com/demos/images/nature-2.jpg" loading="lazy" />
-      </div>
-      <div class="swiper-slide">
-        <Image src="https://swiperjs.com/demos/images/nature-3.jpg"  loading="lazy" />
-      </div>
-      <div class="swiper-slide">
-        <Image src="https://swiperjs.com/demos/images/nature-4.jpg"  loading="lazy" />
-      </div>
-      <div class="swiper-slide">
-        <Image src="https://swiperjs.com/demos/images/nature-5.jpg"  loading="lazy"/>
-      </div>
-      <div class="swiper-slide">
-        <Image src="https://swiperjs.com/demos/images/nature-6.jpg"  loading="lazy"  />
-      </div>
-      <div class="swiper-slide">
-        <Image src="https://swiperjs.com/demos/images/nature-7.jpg"  loading="lazy"  />
-      </div>
-      <div class="swiper-slide">
-        <Image src="https://swiperjs.com/demos/images/nature-8.jpg"  loading="lazy"/>
-      </div>
-      <div class="swiper-slide">
-        <Image src="https://swiperjs.com/demos/images/nature-9.jpg" loading="lazy"  />
-      </div>
+    <div class="section">
+     
     </div>
-    <div class="swiper-pagination"></div>
-  </div> */}
-
   </>)
 })
 
--- src/shared/components/section/galery/galery.tsx
+++ src/shared/components/section/galery/galery.tsx
@@ -1,15 +1,20 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 
 
 import { PageTitle } from "~/shared";
 
+import { Image } from '@unpic/qwik';
+import { imgGaleryUrl } from "~/helpers/config";
+
 
 export const GalerySection = component$(() => {
 
-  
-
-
-
+    const imgs = useSignal<string[]>([
+        'letras.jpg',
+        'letrero luminoso.jpg',
+        'merchandaising.jpg',
+        'roolscream.jpg'
+    ])
 
     return (<>
         <section id="galery" class="bg-[#3d83ff] h-auto w-full flex flex-col justify-around items-center gap-4 text-white">
@@ -20,6 +25,40 @@ export const GalerySection = component$(() => {
                     Sumérgete en un mar de creatividad y pasión plasmados en cada diseño.
                     <br />Te invitamos a disfrutar de nuestra galería de trabajos.
                 </p>
+            </div>
+
+
+            <div id="custom-controls-gallery" class="relative w-full" data-carousel="slide">
+              
+                <div class="relative h-56 overflow-hidden rounded-lg md:h-120">
+
+                    {
+                        imgs.value.map((img) => (
+                            <div  key={img}  class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <Image width="1280" height="1040" src={`${imgGaleryUrl}${img}`} loading="lazy" alt={img}  class="absolute block w-full max-w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+                            </div>
+                        ))
+                    }
+                    
+                </div>
+                <div class="flex justify-center items-center pt-4">
+                    <button type="button" class="flex justify-center items-center mr-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
+                        <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
+                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
+                            </svg>
+                            <span class="sr-only">Previous</span>
+                        </span>
+                    </button>
+                    <button type="button" class="flex justify-center items-center h-full cursor-pointer group focus:outline-none" data-carousel-next>
+                        <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
+                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                            <span class="sr-only">Next</span>
+                        </span>
+                    </button>
+                </div>
             </div>
 
 
