867839fb18e0975947832b95937cf23c1e5ad464
JampoolG <jampoolgarcia@gmail.com>
2023-11-02 06:27:32 -0300

    update contact section

---
+++ src/routes/contact/index.tsx
@@ -0,0 +1,8 @@
+import { component$ } from "@builder.io/qwik";
+
+import { Quote } from "~/shared";
+
+
+export default component$(() => {
+    return <Quote />
+})
--- src/shared/components/section/quote/quote.tsx
+++ src/shared/components/section/quote/quote.tsx
@@ -7,19 +7,19 @@ export const Quote = component$(() => {
     useStylesScoped$(style);
 
     return (<>
-        <section id="quote" class={`contact-section bg-[url('/img/contact.jpg')]`}>
+        <section id="quote" class={`contact-section bg-[url('/img/background.jpg')]`}>
             <div class="contact-info">
-                <span><i class="fa-solid fa-location-dot"></i>Av Bolivia 148, 3er Piso Local 3398 C.C.</span>
-                <span><i class="fas fa-envelope"></i>gerentedeventas@hipublicidad.com</span>
-                <span><i class="fas fa-phone"></i>+51 986771821</span>
-                <span><i class="fa-brands fa-whatsapp"></i>+51 986771821</span>
+                <span class="text-black"><i class="fa-solid fa-location-dot"></i>Av Bolivia 148, 3er Piso Local 3398 C.C.</span>
+                <span class="text-black"><i class="fas fa-envelope"></i>gerentedeventas@hipublicidad.com</span>
+                <span class="text-black"><i class="fas fa-phone"></i>+51 986771821</span>
+                <span class="text-black"><i class="fa-brands fa-whatsapp"></i>+51 986771821</span>
             </div>
             <div class="contact-form">
-                <h2>Contact Us</h2>
+                <h2>Contactó</h2>
                 <form class="contact" action="" method="post">
-                    <input type="text" name="name" class="text-box" placeholder="Enter Your Name" required />
-                    <input type="email" name="email" class="text-box" placeholder="Enter Your Email" required />
-                    <textarea name="menssage" id="menssage" rows={5} class="" placeholder="Enter Your Menssage" required></textarea>
+                    <input type="text" name="name" class="text-box" placeholder="Ingrese su nombre" required />
+                    <input type="email" name="email" class="text-box" placeholder="Ingrese su correo" required />
+                    <textarea name="menssage" id="menssage" rows={5} class="" placeholder="Ingrese su mensaje" required></textarea>
                     <input type="submit" name="submit" class="send-btn" value="Enviar" />
                 </form>
             </div>
