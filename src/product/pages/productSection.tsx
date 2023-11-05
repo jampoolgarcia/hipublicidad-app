0482119e9f00bdb9bd417db1bb634e55fd6d5519
JampoolG <jampoolgarcia@gmail.com>
2023-11-05 08:33:49 -0300

    update product

--- src/product/pages/productSection.tsx
+++ src/product/pages/productSection.tsx
@@ -14,12 +14,13 @@ export const ProductSection =  component$(() => {
     const randomProducts = useProductRandom(products, 12);
 
     return (<>
-            <section id="product" class="flex flex-col gap-4 justify-center my-6 rt  text-center w-full">
+            <section id="product" class="flex flex-col gap-4 justify-center my-6 rt text-center w-full">
                 <PageTitle title="Nuestros Productos"></PageTitle>
                 <p class="text-sm xl:text-base mt-2 mb-4 text-gray-600">
                  Transforma tus diseños con la calidad de nuestros productos. <br /> Contáctanos para realizar tu <a href="#quote" class="text-bold">cotización</a>.<br />
-                 <Link class="font-primary font-bold pulse" href="/products">Ver todos</Link>
                 </p>
+
+                <Link class="btn" href="/products">Ver más</Link>
 
                 <div class="grid grid-cols-1  sm:grid-cols-3  md:grid-cols-4 gap-4 mt-4">
                 {
